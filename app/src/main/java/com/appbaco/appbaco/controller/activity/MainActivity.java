632b417c03873aa35478eb492c9f0e275860ce6a
emilioaraujo <emilio.araujo@gmail.com>
2017-01-19 13:22:40 +0100

    modificado cabecera navegacion

--- app/src/main/java/com/appbaco/appbaco/controller/activity/MainActivity.java
+++ app/src/main/java/com/appbaco/appbaco/controller/activity/MainActivity.java
@@ -92,17 +92,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_mainmenu) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_count) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_categories) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_transactions) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_config) {
+
+        }else if (id == R.id.nav_about) {
 
         }
 
