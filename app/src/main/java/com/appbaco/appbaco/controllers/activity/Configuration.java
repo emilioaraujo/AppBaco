bce7f0bfafcd6e10ca5a849bcb374c8dce6c467e
emilioaraujo <emilio.araujo@gmail.com>
2017-01-25 13:35:12 +0100

    About

--- app/src/main/java/com/appbaco/appbaco/controllers/activity/Configuration.java
+++ app/src/main/java/com/appbaco/appbaco/controllers/activity/Configuration.java
@@ -60,12 +60,14 @@ public class Configuration extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_configuration, container, false);
         return inflater.inflate(R.layout.fragment_configuration, container, false);
     }
 
